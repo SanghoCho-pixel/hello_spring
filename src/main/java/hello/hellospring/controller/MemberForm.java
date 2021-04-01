20ee2e5c7d1f633d3fdaaeaf70dde9fc0d7ab149
SanghoCho <shxho0476@gmail.com>
2021-04-02 01:58:41 +0900

    회원가입 및 등록 예제 스프링 웹 MVC(1/2)

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String abc) {
+        this.name = abc;
+    }
+}
